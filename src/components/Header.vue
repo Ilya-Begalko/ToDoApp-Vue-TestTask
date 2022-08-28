a6e0c4b6af2229f764d3ca385c510b87d74a8e93
Ilya Begalko <ilja.begalko@yandex.ru>
2022-08-28 14:02:09 +0600

    add header component and delete excess

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="header">
+    To do list
+  </div>
+</template>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap');
+  .header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    font-family: 'Montserrat';
+    font-weight: 700;
+    font-size: 24px;
+  }
+</style>
